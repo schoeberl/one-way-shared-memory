6ff78c89dc1e076c4d133951791ea271c2becd3f
Martin Schoeberl <martin@jopdesign.com>
2017-09-14 18:03:21 +0200

    Better aggreation of dummy results

--- src/main/scala/oneway/OneWayMem.scala
+++ src/main/scala/oneway/OneWayMem.scala
@@ -14,18 +14,25 @@ import Const._
  */
 class OneWayMem(n: Int) extends Module {
   val io = new Bundle {
-    val dout = Vec(UInt(width = 32), n*n).asOutput
+    val dout = UInt(width = 32).asOutput
   }
+
+  // Dummy output keep hardware generated
+  val dout = Reg(next = Vec(UInt(width = 32), n * n))
 
   val net = Module(new Network(n))
 
-  for (i <- 0 until n*n) {
-    val node = Module(new Node(i))
+  for (i <- 0 until n * n) {
+    val node = Module(new DummyNode(i))
     // how do we avoid confusing in/out names?
     net.io.local(i).in := node.io.local.out
     node.io.local.in := net.io.local(i).out
-    io.dout(i) := node.io.dout
+    dout(i) := node.io.dout
   }
+
+  val or = dout.fold(UInt(0))(_ | _)
+  io.dout := Reg(next = or)
+
 }
 
 object OneWayMem extends App {
