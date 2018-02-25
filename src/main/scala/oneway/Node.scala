d832cae366d069e499861ed3c26e37ea60851420
Martin Schoeberl <martin@jopdesign.com>
2018-02-25 19:15:25 +0100

    Counters dependent on valid table or signal

--- src/main/scala/oneway/Node.scala
+++ src/main/scala/oneway/Node.scala
@@ -21,51 +21,64 @@ class Node(n: Int, size: Int) extends Module {
   val st = Schedule.getSchedule(n)
   val scheduleLength = st._1.length
   val validTab = Vec(st._2.map(Bool(_)))
-  // TODO: get the valid slots from the schedule for counter increments
-  // Counters will be different for send and receive
-  
+
   val regTdmCounter = Reg(init = UInt(0, log2Up(scheduleLength)))
-  val end = regTdmCounter === UInt(scheduleLength-1)
+  val end = regTdmCounter === UInt(scheduleLength - 1)
   regTdmCounter := Mux(end, UInt(0), regTdmCounter + UInt(1))
-  
-  val regAddrLower = Reg(init = UInt(0, log2Up(scheduleLength)))
-  
-  // For quicker testing use only 4 words per connection
-  // TODO: get this constant from somewhere
-  val regAddrUpper = Reg(init = UInt(0, 2))
-  when (end) {
-    regAddrUpper := regAddrUpper + UInt(1)
-    regAddrLower := UInt(0)
-  }
-  
-  val valid = validTab(regTdmCounter)
-  when (valid) {
-    regAddrLower := regAddrLower + UInt(1)
-  }
 
-  // TODO: fix the addresses and the write enables
-  val address = Cat(regTdmCounter, regAddrUpper)
-
-  // Receive data from the NoC
-  val memRx = Module(new DualPortMemory(size))
-  
-  memRx.io.port.wrAddr := address
-  memRx.io.port.wrData := io.local.in.data
-  memRx.io.port.wrEna := Bool(true)
-
-  memRx.io.port.rdAddr := io.memPort.rdAddr
-  io.memPort.rdData := memRx.io.port.rdData
+  val nChannles = n * n - 1
 
   // Send data to the NoC
-  val memTx = Module(new DualPortMemory(size))
+  val regTxAddrLower = Reg(init = UInt(0, log2Up(scheduleLength)))
+  // For quicker testing use only 4 words per connection
+  // TODO: get this constant from somewhere
+  val regTxAddrUpper = Reg(init = UInt(0, 2))
 
+  val valid = validTab(regTdmCounter)
+  when(valid) {
+    when(regTxAddrUpper === UInt(nChannles - 1)) {
+      regTxAddrUpper := UInt(0)
+      regTxAddrLower := regTxAddrLower + UInt(1)
+    }.otherwise {
+      regTxAddrUpper := regTxAddrUpper + UInt(1)
+    }
+  }
+
+  val txAddr = Cat(regTxAddrUpper, regTxAddrLower)
+
+  val memTx = Module(new DualPortMemory(size))
   memTx.io.port.wrAddr := io.memPort.wrAddr
   memTx.io.port.wrData := io.memPort.wrData
   memTx.io.port.wrEna := io.memPort.wrEna
-
-  memTx.io.port.rdAddr := address
+  memTx.io.port.rdAddr := txAddr
   io.local.out.data := memTx.io.port.rdData
   // TDM schedule starts one cycle late for read data delay
-  // io.local.out.valid := RegNext(valid)
+  io.local.out.valid := RegNext(valid)
+
+  // Receive data from the NoC  
+  val regRxAddrLower = Reg(init = UInt(0, log2Up(scheduleLength)))
+  // For quicker testing use only 4 words per connection
+  // TODO: get this constant from somewhere
+  val regRxAddrUpper = Reg(init = UInt(0, 2))
+
+  val validRx = io.local.in.valid
   
+  when(validRx) {
+    when(regRxAddrUpper === UInt(nChannles - 1)) {
+      regRxAddrUpper := UInt(0)
+      regRxAddrLower := regRxAddrLower + UInt(1)
+    }.otherwise {
+      regRxAddrUpper := regRxAddrUpper + UInt(1)
+    }
+  }
+
+  val rxAddr = Cat(regRxAddrUpper, regRxAddrLower)
+
+  val memRx = Module(new DualPortMemory(size))
+  memRx.io.port.wrAddr := txAddr
+  memRx.io.port.wrData := io.local.in.data
+  memRx.io.port.wrEna := validRx
+  memRx.io.port.rdAddr := io.memPort.rdAddr
+  io.memPort.rdData := memRx.io.port.rdData
+
 }
